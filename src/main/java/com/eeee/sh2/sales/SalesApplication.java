efb09f7738ba016a36ae2d2d17efb94a7579a844
Charles Shi <charles.qld@gmail.com>
2017-07-27 16:52:28 +1000

    replace with an empty project

---
+++ src/main/java/com/eeee/sh2/sales/SalesApplication.java
@@ -0,0 +1,12 @@
+package com.eeee.sh2.sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesApplication.class, args);
+	}
+}
